6f2c470a5ab6acfc6c5008b7b4d46baaca234f8c
Ruthan <rcfreese@gmail.com>
2013-08-18 21:12:47 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,12 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-               :text => "Bento App | Home")
+               :text => "Bento App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
