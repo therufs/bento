4c494dd30bebf573ba9a13909733fa0b753d068c
Ruthan <rcfreese@gmail.com>
2013-08-20 09:52:20 -0400

    Make routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Bento'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Bento')
     end
   
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                :text => "Bento App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end
   end
@@ -22,12 +22,12 @@ describe "StaticPages" do
 
   describe "Help page" do
     it "should have the content 'HALP'"do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('HALP')
     end
   
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                :text => "Help")
     end
@@ -35,13 +35,13 @@ describe "StaticPages" do
 
   describe "About page" do 
     it "should have the content 'About Bento'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Bento')
     end
   
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                :text => "Bento App | About")
     end
@@ -50,12 +50,12 @@ describe "StaticPages" do
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
                     text: " Contact")
     end
