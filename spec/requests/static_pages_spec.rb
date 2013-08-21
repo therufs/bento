fbbf0d77e322eaaccbca1d73875710af255f0ad2
Ruthan <rcfreese@gmail.com>
2013-08-20 21:56:55 -0400

    Improve tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,34 +4,52 @@ describe "StaticPages" do
 
 subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content 'Bento' }
-    it { should have_selector('title',
-               text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',  text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
   end
 
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { 'Bento App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should have_content 'Bento' }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
 
   describe "Help page" do
     before { visit help_path }
     it { should have_content 'HALP' }
-    it { should have_selector 'title',
-               :text => "Help" }
+    it { should have_selector 'title', :text => "Help" }
   end
 
   describe "About page" do 
     before {  visit about_path }
     it { should have_content 'About Bento' }
-    it { should have_selector 'title',
-               :text => "Bento App | About" }
+    it { should have_selector 'title', :text => "Bento App | About" }
   end
 
   describe "Contact page" do
     before {  visit contact_path }
     it { should have_selector 'h1', text: 'Contact' }
-    it { should have_selector 'title',
-                    text: "Contact" }
+    it { should have_selector 'title', text: "Contact" }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Bento')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "bento app"
+    page.should have_selector 'title', text: 'Bento App'
+  end
+
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 def full_title(page_title)
   base_title = "Bento App"
     if page_title.empty?
